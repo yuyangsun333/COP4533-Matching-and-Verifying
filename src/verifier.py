06d100329e9a199f45d71804c846d4be0cdf3276
Jerry <lijunhao@ufl.edu>
2026-01-25 23:56:09 -0500

    verifier update

--- src/verifier.py
+++ src/verifier.py
@@ -73,11 +73,12 @@ def get_rank(prefs: List[List[int]], n: int) -> List[List[int]]:
     """
     Convert ordered preference lists into a rank lookup table.
     
-    :param prefs: Description
+    :param prefs: Preference lists from the input file.
     :type prefs: List[List[int]]
-    :param n: Description
+    :param n: The total number of hospitals (which equals the number of students).
     :type n: int
-    :return: Description
+    :return: A 2D rank table where rank[x][y] gives how agent x ranks partner y.
+             Lower values mean higher preference (0 = best).
     :rtype: List[List[int]]
     """
     # Create (n+1) x (n+1) table, index 0 is unused
@@ -91,3 +92,50 @@ def get_rank(prefs: List[List[int]], n: int) -> List[List[int]]:
             rank[x][y] = index # y is the 'index'-th choice of agent x
     
     return rank
+
+def verify_stability(n: int,
+                     hospital_prefs: List[List[int]],
+                     student_prefs: List[List[int]],
+                     pairs: List[Tuple[int, int]]) -> Tuple[bool, str]:
+    """
+    Verify whether a given matching is stable.
+    
+    :param n: The total number of hospitals (which equals the number of students).
+    :type n: int
+    :param hospital_prefs: Hospital preference lists read from the input file.
+    :type hospital_prefs: List[List[int]]
+    :param student_prefs: Student preference lists read from the input file.
+    :type student_prefs: List[List[int]]
+    :param pairs: matching output, list of (h, s)
+    :type pairs: List[Tuple[int, int]]
+    :return: (True, "STABLE") if no blocking pair exists,
+             otherwise (False, "UNSTABLE: blocking pair (h, s)").
+    :rtype: Tuple[bool, str]
+    """
+    # Must be valid first
+    is_valid, msg = verify_validity(n, pairs)
+    if not is_valid:
+        return False, msg
+    
+    # Build matching maps
+    match_h, match_s = get_match(n, pairs)
+
+    # Build rank lookup tables
+    hospital_rank = get_rank(hospital_prefs, n) # hospital_rank[h][s]
+    student_rank = get_rank(student_prefs, n) #student_rank[s][h]
+
+    # For each hospital h, only consider students it prefers over its current match.
+    for h in range(1, n + 1):
+        current_s = match_h[h]
+
+        # Iterate hospital h's preference list untile reaching current_s.
+        for s in hospital_prefs[h - 1]: # h - 1 represent current hospital
+            if s == current_s:
+                break
+            
+            current_h = match_s[s] # current_h: student's currently matched hospital
+            # student s prefers h over its current hospital.
+            if student_rank[s][h] < student_rank[s][current_h]:
+                return False, f"UNSTABLE: Blocking pair ({h}, {s})"
+            
+    return True, "STABLE"
